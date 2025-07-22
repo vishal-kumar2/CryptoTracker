b7beca0f1cda2685d93d83abe91f797dc87f8122
vishal-kumar2 <vishalk73877@gmail.com>
2025-07-22 17:52:05 +0530

    add hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,67 @@
-import React, { useContext } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo 4.png'
-import arrow_icon from '../../assets/arrow_icon.png'
-import { CoinContext } from '../../context/CoinContext'
-import { Link } from 'react-router-dom'
-
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo 4.png';
+import arrow_icon from '../../assets/arrow_icon.png';
+import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const {setCurrency}=useContext(CoinContext)
-
-  const currencyHandler=(event)=>{
-    switch (event.target.value)
-    {
-      case "usd":{
-        setCurrency({name:"usd",symbol:"$"})
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case 'usd':
+        setCurrency({ name: 'usd', symbol: '$' });
         break;
-      }
-      case "eur":{
-        setCurrency({name:"eur",symbol:"€"});
+      case 'eur':
+        setCurrency({ name: 'eur', symbol: '€' });
         break;
-      }
-      case "inr":{
-        setCurrency({name:"inr",symbol:"₹"});
+      case 'inr':
+        setCurrency({ name: 'inr', symbol: '₹' });
         break;
-      }
-      
-      default :{
-        setCurrency({name:"usd",symbol:"$"});
+      default:
+        setCurrency({ name: 'usd', symbol: '$' });
         break;
-      }
     }
-  }
+  };
+
   return (
     <div className='navbar'>
-      <Link to={'/'}>
-        <img src={logo} alt="" className='logo'/>
+      <Link to='/'>
+        <img src={logo} alt="" className='logo' />
       </Link>
-      
-      <ul>
-        <Link to={'/'}><li>Home</li></Link>
-        <Link to={'/features'}><li>Features</li></Link>
-        <Link to={'/pricing'}><li>Pricing</li></Link>
-        <Link to={'/blog'}><li>Blog</li></Link>
-        
+
+      {/* Navigation Links */}
+      <ul className={`nav-links ${menuOpen ? 'mobile-show' : ''}`}>
+        <Link to='/' onClick={() => setMenuOpen(false)}><li>Home</li></Link>
+        <Link to='/features' onClick={() => setMenuOpen(false)}><li>Features</li></Link>
+        <Link to='/pricing' onClick={() => setMenuOpen(false)}><li>Pricing</li></Link>
+        <Link to='/blog' onClick={() => setMenuOpen(false)}><li>Blog</li></Link>
       </ul>
+
+      {/* Right Section: Currency + SignUp + Menu */}
       <div className="nav-right">
-        <select onChange={currencyHandler} >
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
-          
         </select>
-        <Link to={'/signup'}><button>Sign Up <img src={arrow_icon} alt="" /></button></Link>
-        
-      </div>
-      
-    </div>
-  )
-}
+        <Link to='/signup'>
+          <button>
+            Sign Up <img src={arrow_icon} alt="" />
+          </button>
+        </Link>
 
-export default Navbar
+        {/* Hamburger now inside nav-right */}
+        <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          <span className='bar'></span>
+          <span className='bar'></span>
+          <span className='bar'></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
